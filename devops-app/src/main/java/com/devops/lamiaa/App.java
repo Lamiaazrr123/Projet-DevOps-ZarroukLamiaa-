84c897ab393a41fd1d3337e754356013ae5d6721
Lamiaa Zarrouk <lamiaazarrouk@gmail.com>
2026-01-11 18:40:35 +0100

    Ajout application Maven avec message DevOps

---
+++ devops-app/src/main/java/com/devops/lamiaa/App.java
@@ -0,0 +1,8 @@
+package com.devops.lamiaa;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+
+    }
+}
